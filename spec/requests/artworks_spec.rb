fbee5376b4aebcf8e8f0d0d878f90084dec6eee3
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-06-30 18:36:08 -0700

    Deleted unused files.

--- spec/requests/artworks_spec.rb
+++ spec/requests/artworks_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Artworks', type: :request do
   describe 'GET /artworks' do
-    it 'works! (now write some real specs)' do
+    it 'works!' do
       get artworks_path
       expect(response).to have_http_status(200)
     end
