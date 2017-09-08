eeb6e8275ee9fd098dd6a7c6159cead2e2f33259
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-09-08 12:21:12 -0400

    Simplified artwork/photo models.

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -6,14 +6,10 @@ class Artwork < ApplicationRecord
   friendly_id :title, use: :slugged
   has_attached_file :image, styles: { large: '800x', medium: '400x' }
 
-  validates :title, presence: true
-  validates :slug, presence: true
-  validates :media, presence: true
-  validates :size, presence: true
+  validates :title, :slug, :media, :size, presence: true
   validates :price, presence: true, if: :for_sale?
   validates :artwork_type, inclusion: { in: %w(copic painting sketch) }
-  validates :featured, inclusion: { in: [true, false] }
-  validates :for_sale, inclusion: { in: [true, false] }
+  validates :featured, :for_sale, inclusion: { in: [true, false] }
 
   validate :slug_is_parameterized_title
 
